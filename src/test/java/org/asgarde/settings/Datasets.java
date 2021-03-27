01a52e10424d7b2e67e872ced1e5305ccee07f4e
Mazlum Tosun <mazlum.tosun-extern@renault.com>
2021-03-27 02:11:11 +0100

    Removes unused import.

--- src/test/java/org/asgarde/settings/Datasets.java
+++ src/test/java/org/asgarde/settings/Datasets.java
@@ -11,8 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
-
 import static java.util.stream.Collectors.toList;
 
 /**
